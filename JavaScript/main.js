ca6633ab4e3bb181cbb9e1b7860911624e720773
LordStickyNote <asmastorp@gmail.com>
2026-02-09 01:05:56 +0100

    final

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,3 +1,3 @@
-import { assignmentsOutput, navTabs } from "/JavaScript/assignments.js";
+import { assignmentsOutput, navTabs } from "./assignments.js";
 assignmentsOutput(document.getElementById("card-grid-container"));
 navTabs(document.querySelector("nav"));
--- assignments/assignment1/assignment1.js
+++ assignments/assignment1/assignment1.js
@@ -1,3 +1,3 @@
-import { assignmentPage, navTabs} from "/JavaScript/assignments.js";
+import { assignmentPage, navTabs} from "../../JavaScript/assignments.js";
 assignmentPage(0, document.getElementById("info-container"));
 navTabs(document.querySelector("nav"), 0);
